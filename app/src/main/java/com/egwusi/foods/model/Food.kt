12f5502be8022018200c08a30b8979fa2d2e641c
bfa5 <sng0515-private@yahoo.fr>
2021-08-11 13:04:29 -0400

    Room Database

---
+++ app/src/main/java/com/egwusi/foods/model/Food.kt
@@ -0,0 +1,14 @@
+package com.egwusi.foods.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+
+@Entity(tableName = "Foods")
+data class Food(@PrimaryKey @field:SerializedName("id") val id: Int,
+                     @field:SerializedName("name") val name: String = "",
+                     @field:SerializedName("description") val description: String = "",
+                     @field:SerializedName("image") val image: String = "",
+                     @field:SerializedName("price") val price: Int = 0
+                     )
